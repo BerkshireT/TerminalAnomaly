b8e2af28e831b74269b9364ce8f3b1e6813cc3dd
TerminalAnomaly <berkshiret1@udayton.edu>
2019-03-07 16:27:08 -0500

    Default Vuetify, FontAwesome

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import Vue from 'vue'
+import './plugins/vuetify'
 import App from './App.vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faDumpster } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faDumpster)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
