758d9001236ea1f341a46fe7088d558287afec78
Tyler Berkshire <berkshiret1@udayton.edu>
2019-03-27 01:07:42 -0400

    Wrote some bckrnd, mobile friendly, layout final

--- src/components/AboutTile.vue
+++ src/components/AboutTile.vue
@@ -1,14 +1,69 @@
 <template>
-    <v-card class='py-2 grey darken-4'>
+    <v-card class="py-2 grey darken-4">
         <v-layout>
-            <v-flex xs12 v-if="alignProp === 'center'">
-                <h1 class="pa-3 white--text text-xs-center">{{ textProp }}</h1>
-            </v-flex>
-            <v-flex xs12 v-else-if="alignProp === 'left'">
-                <h1 class="pa-3 white--text text-xs-left">{{ textProp }}</h1>
-            </v-flex>
-            <v-flex xs12 v-else>
-                <h1 class="pa-3 white--text text-xs-right">{{ textProp }}</h1>
+            <v-flex xs12>
+                <div v-if="alignProp === 'center'" class="hidden-sm-and-down">
+                    <div class="pa-3 text-xs-center display-2 font-weight-thin indigo--text text--lighten-3">{{ titleProp }}</div>
+                    <v-divider dark class="py-1" />
+                    <v-layout row wrap>
+                        <v-flex xs1 />
+                        <v-flex xs10>
+                            <v-img :src="require('@/assets/' + picProp)" height="400" class="ma-3 pb-1" contain/>
+                        </v-flex>
+                        <v-flex xs1 />
+                        <v-flex xs12 v-for="paragraph in textProp" :key="paragraph">
+                            <div class="pa-2 mx-3 text-xs-center subheading white--text">{{ paragraph }}</div>
+                        </v-flex>
+                    </v-layout>
+                </div>
+                <div v-else-if="alignProp === 'left'" class="hidden-sm-and-down">
+                    <div class="pa-3 text-xs-left display-2 font-weight-thin indigo--text text--lighten-3">{{ titleProp }}</div>
+                    <v-divider dark class="py-1" />
+                    <v-layout row wrap>
+                        <v-flex xs6>
+                            <v-layout column wrap>
+                                <v-flex xs6 v-for="paragraph in textProp" :key="paragraph">
+                                    <div class="pa-2 mx-3 text-xs-center subheading white--text">{{ paragraph }}</div>
+                                </v-flex>
+                            </v-layout>
+                        </v-flex>
+                        <v-flex xs6>
+                            <v-img :src="require('@/assets/' + picProp)" height="400" class="ma-3" contain />
+                        </v-flex>
+                    </v-layout>
+                </div>
+                <div v-else class="hidden-sm-and-down">
+                    <div class="pa-3 text-xs-right display-2 font-weight-thin indigo--text text--lighten-3">{{ titleProp }}</div>
+                    <v-divider dark class="py-1" />
+                    <v-layout row wrap>
+                        <v-flex xs6>
+                            <v-img :src="require('@/assets/' + picProp)" height="400" class="ma-3" contain />
+                        </v-flex>
+                        <v-flex xs6>
+                            <v-layout column wrap>
+                                <v-flex xs6 v-for="paragraph in textProp" :key="paragraph">
+                                    <div class="pa-2 mx-3 text-xs-center subheading white--text">{{ paragraph }}</div>
+                                </v-flex>
+                            </v-layout>
+                        </v-flex>
+                    </v-layout>
+                </div>
+                <!-- Mobile -->
+                <div class="hidden-md-and-up">
+                    <div class="pa-3 text-xs-center display-2 font-weight-thin indigo--text text--lighten-3">{{ titleProp }}</div>
+                    <v-divider dark class="py-1" />
+                    <v-layout row wrap>
+                        <v-flex xs1 />
+                        <v-flex xs10>
+                            <v-img :src="require('@/assets/' + picProp)" height="400" class="ma-3 pb-1" contain/>
+                        </v-flex>
+                        <v-flex xs1 />
+                        <v-flex xs12 v-for="paragraph in textProp" :key="paragraph">
+                            <div class="pa-1 mx-3 text-xs-center subheading white--text">{{ paragraph }}</div>
+                            <br />
+                        </v-flex>
+                    </v-layout>
+                </div>
             </v-flex>
         </v-layout>
     </v-card>
@@ -19,7 +74,7 @@ export default {
     props:{
         titleProp: {
             required: true,
-            default: 'Title'
+            default: 'TEST'
         },
         alignProp: {
             required: false,
@@ -27,9 +82,18 @@ export default {
         },
         textProp: {
             required: true,
-            default: 'lorem ipsum'
+            default: [ 'lorem ipsum' ]
+        },
+        picProp: {
+            required: true,
+            default: 'comingsoon.jpg'
         }
     }
 }
 </script>
 
+<style>
+.theme--dark.v-divider {
+    border-color: rgba(159, 168, 218) !important; 
+}
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -5,7 +5,7 @@
             <v-layout row wrap>
                 <v-flex xs12>
                     <v-card :class="'grey darken-4 ' + theme.color">
-                        <v-layout>
+                        <v-layout hidden-xs-only>
                             <v-flex xs5>
                                 <v-img :src="require('@/assets/aboutimage.png')" contain height="300px" />
                             </v-flex>
@@ -20,10 +20,26 @@
                                 </v-card-title>
                             </v-flex>
                         </v-layout>
+                        <!-- Mobile Only -->
+                        <v-layout column hidden-sm-and-up>
+                            <v-flex xs5>
+                                <v-img :src="require('@/assets/aboutimage.png')" contain height="300px" />
+                            </v-flex>
+                            <v-flex xs7>
+                                <v-card-title primary-title>
+                                    <div>
+                                        <div class="display-2 font-weight-thin">Tyler Berkshire</div>
+                                        <v-divider dark />
+                                        <div class="display-1 font-weight-thin white--text">Computer Programmer</div>
+                                        <div class="display-1 font-weight-thin white--text">University of Dayton CPS '19</div>
+                                    </div>
+                                </v-card-title>
+                            </v-flex>
+                        </v-layout>
                     </v-card>
                     <v-divider dark class="py-1" />
                     <v-flex v-for="tile in cards" :key="tile.title">
-                        <AboutTile :titleProp="tile.title" :alignProp="tile.align" :textProp="tile.text" />
+                        <AboutTile :titleProp="tile.title" :alignProp="tile.align" :textProp="tile.text" :picProp="tile.pic" />
                         <v-divider dark class="py-1" />
                     </v-flex>
                 </v-flex>
@@ -47,9 +63,33 @@ export default {
                 image: 'ta-nav-in.png' 
             },
             cards: [
-                { title: 'history', align: 'center', text: 'history section' },
-                { title: 'programming', align: 'left', text: 'programming section' },
-                { title: 'hobbies', align: 'right', text: 'hobbies section'}
+                { title: 'Background', align: 'center', pic: 'gal1.jpg', 
+                    text: [
+                        'My name is Tyler Berkshire and I was born and raised in the greater Dayton, '
+                        + 'Ohio area. I attended Bethel Local Schools for my primary and secondary '
+                        + 'education. I was a part of the Varsity Bethel Men\'s Soccer Team for all four '
+                        + 'years of my secondary education and I received my Honors Diploma in May of 2016 ' 
+                        + 'with a 4.10 GPA. After high school, I attended the University of Dayton where '
+                        + 'I am currently enrolled in the Computer Science department to earn my Bachelor '
+                        + 'of Science degree with a minor in Psychology. Some of my favorite courses I took '
+                        + 'were Operating Systems, Data Structures, Computer Organzation & Architecture, '
+                        + 'Cognition, Forensic Psychology, Creative Writing, and Descriptive Astronomy. '
+                        + 'I plan on graduating in December of 2020. During my time at UD, I also co-founded '
+                        + 'and served as president of the Super Smash Brothers Club which has become one of the '
+                        + 'fastest growing student organizations at the university since its creation in 2017.',
+
+                        'TESTING'
+                    ]},
+                { title: 'Programming Experience', align: 'left', pic: 'comingsoon.jpg',
+                    text: [
+                        'Hey',
+                        'Testing'
+                    ]},
+                { title: 'Hobbies', align: 'right', pic: 'comingsoon.jpg',
+                    text: [
+                        'Hey',
+                        'Testing'
+                    ]}
             ]
         }
     }
