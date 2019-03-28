c733da77a408e3a647db225789b0672623eab6ea
Tyler Berkshire <berkshiret1@udayton.edu>
2019-03-27 23:25:35 -0400

    Created Footer comp, added to App

---
+++ src/components/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+   <v-footer dark height="auto">
+      <v-flex pa-3 text-xs-center>
+         &copy; {{ new Date().getFullYear() }} - <strong>Tyler Berkshire</strong>
+      </v-flex>
+   </v-footer>
+</template>
