30de299f0802b5bdee526551b5b03d16e12c0da5
TerminalAnomaly <berkshiret122@gmail.com>
2020-12-21 22:08:40 -0500

    Smash Search setup;

---
+++ src/scripts/SmashGG.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+function Request(game, location, radius) {
+   console.log(game)
+   console.log(location)
+   console.log(radius)
+
+   let key = process.env.VUE_APP_SMASHGG_API
+   console.log(key)
+}
+
+export default Request
+export { Request }
---
+++ src/views/SuperSmashSearch.vue
@@ -0,0 +1,74 @@
+<template>
+  <v-container fluid fill-height class="smash">
+    <v-layout wrap text-xs-center justify-center>
+      <v-flex xs12>
+        <Topbar :inner="true" :image="theme.image" :color="theme.color" />
+      </v-flex>
+      <v-flex xs12>
+        <div class="aesthetic-windows-95-loader">
+           <div></div>
+           <div></div>
+           <div></div>
+        </div>
+      </v-flex>
+      <v-flex xs12 pa-2 text-xs-center :class="$vuetify.breakpoint.smAndUp? 'credit' : 'credit-m'">
+         <span>
+            Styling on this page utilizes <a href="https://torch2424.github.io/aesthetic-css/">aesthetic-css</a>. Check them out!
+         </span>
+    </v-flex>
+    </v-layout>
+    <Footer />
+  </v-container>
+</template>
+
+<script>
+import Footer from '@/components/Footer.vue'
+import Topbar from '@/components/Topbar.vue'
+//import Request from '@/scripts/SmashGG.js'
+
+export default {
+  title: 'SuperSmashSearch',
+  components: { Footer, Topbar },
+  data () {
+     return {
+        theme: { color: '#b967ff', image: 'ta-purple.gif' }
+     }
+  },
+  methods: {
+
+  },
+  created() {
+     let twitterFeed = document.createElement("script");
+     twitterFeed.setAttribute("src", "https://platform.twitter.com/widgets.js");
+     document.head.appendChild(twitterFeed);
+  }
+}
+</script>
+
+<style scoped>
+.smash {
+  width: 100%;
+  height: 100%;
+  background: url(../assets/backgrounds/grid-purple.png);
+  background-color: black;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-attachment: fixed;
+  color: white;
+}
+
+a {
+  color: inherit;
+  text-decoration: underline;
+}
+
+.credit {
+   letter-spacing: 0.3em;
+   font-family: 'OCR-A', 'Courier', monospace;
+}
+
+.credit-m {
+   font-size: 10px;
+}
+</style>
